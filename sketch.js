7f2c8256d51a29d15145f43a382bbf0466d82aab
rdhimal007 <73763017+rdhimal007@users.noreply.github.com>
2022-09-28 17:26:09 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
 
 let clrs = [
-// '#E27F34', //top 0
-'#B7410E',
-// '#FEF254', //middle top 1
-	'#d1b8b3',  // new color 
-	'#d1e231', // light green
+'#E27F34', //top 0
+
+'#FEF254', //middle top 1
 '#A36C33', //middle strip 2
 '#042060', //middle bottom 3
 '#3778AE' //bottom 4
